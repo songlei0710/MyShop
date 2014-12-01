5f3eb2aef635c3fbaa751268d4a20b967d63675c
liujiayin <506465308@qq.com>
2014-12-01 11:22:59 +0800

    刘佳音--实体修正2

--- src/net/hzjxy/myshop/entity/Function.java
+++ src/net/hzjxy/myshop/entity/Function.java
@@ -8,9 +8,57 @@ import java.util.Date;
  */
 public class Function {
     private String id;                  //编号
-    private String visiturl;            //虚拟路径
-    private String visitmethodname;     //虚拟方法名
-    private String functionname;        //功能名称
-    private Date createtime;            //创建时间
-    private String moduleid;            //工程Id
+    private String visitUrl;            //虚拟路径
+    private String visitMethodName;     //虚拟方法名
+    private String functionName;        //功能名称
+    private Date createTime;            //创建时间
+    private String moduleId;            //工程Id
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVisitUrl() {
+        return visitUrl;
+    }
+
+    public void setVisitUrl(String visitUrl) {
+        this.visitUrl = visitUrl;
+    }
+
+    public String getVisitMethodName() {
+        return visitMethodName;
+    }
+
+    public void setVisitMethodName(String visitMethodName) {
+        this.visitMethodName = visitMethodName;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(String moduleId) {
+        this.moduleId = moduleId;
+    }
 }
---
+++ src/net/hzjxy/myshop/entity/GoodsBlinked.java
@@ -0,0 +1,118 @@
+package net.hzjxy.myshop.entity;
+
+import java.util.Date;
+
+/**
+ * 关联商品
+ * Created by 磊 on 2014/11/30.
+ */
+public class GoodsBlinked {
+    private String id;                      //编号
+    private String mainGoodsId;             //主商品Id
+    private String beLinkedProductInfo;     //关联商品信息
+    private String mode;
+    private String state;
+    private String creatorId;
+    private Date createTime;
+    private Date updateTime;
+    private String sxLinkedGoodsId;
+    private String mainProductId;
+    private String shopId;
+    private String mainGoodsName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMainGoodsId() {
+        return mainGoodsId;
+    }
+
+    public void setMainGoodsId(String mainGoodsId) {
+        this.mainGoodsId = mainGoodsId;
+    }
+
+    public String getBeLinkedProductInfo() {
+        return beLinkedProductInfo;
+    }
+
+    public void setBeLinkedProductInfo(String beLinkedProductInfo) {
+        this.beLinkedProductInfo = beLinkedProductInfo;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getSxLinkedGoodsId() {
+        return sxLinkedGoodsId;
+    }
+
+    public void setSxLinkedGoodsId(String sxLinkedGoodsId) {
+        this.sxLinkedGoodsId = sxLinkedGoodsId;
+    }
+
+    public String getMainProductId() {
+        return mainProductId;
+    }
+
+    public void setMainProductId(String mainProductId) {
+        this.mainProductId = mainProductId;
+    }
+
+    public String getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(String shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getMainGoodsName() {
+        return mainGoodsName;
+    }
+
+    public void setMainGoodsName(String mainGoodsName) {
+        this.mainGoodsName = mainGoodsName;
+    }
+}
