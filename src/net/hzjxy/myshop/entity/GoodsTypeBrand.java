6e5e92b72caaa54b3056a5d8186b8f2bf3fe7237
wendy521 <www.15570316@qq.com>
2014-12-04 10:45:06 +0800

    wendy123

--- src/net/hzjxy/myshop/entity/GoodsTypeBrand.java
+++ src/net/hzjxy/myshop/entity/GoodsTypeBrand.java
@@ -1,7 +1,7 @@
 package net.hzjxy.myshop.entity;
 
 /**
- * Created by 磊 on 2014/11/30...
+ * Created by 磊 on 2014/11/30.
  */
 public class GoodsTypeBrand {
     private String goodsTypeBrandTid;
--- src/net/hzjxy/myshop/entity/WebSiteMsg.java
+++ src/net/hzjxy/myshop/entity/WebSiteMsg.java
@@ -2,7 +2,7 @@ package net.hzjxy.myshop.entity;
 
 import java.util.Date;
 /**
- * 网站消息..
+ * 网站消息
  * Created by 磊 on 2014/11/30.
  */
 public class WebSiteMsg {
