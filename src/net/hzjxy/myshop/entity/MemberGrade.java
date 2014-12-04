214c51080d21f2a573bce23f9114373a8a93c98b
linchunlei <664390026@qq.com>
2014-12-04 09:11:24 +0800

    林春蕾--封装实体

--- src/net/hzjxy/myshop/entity/MemberGrade.java
+++ src/net/hzjxy/myshop/entity/MemberGrade.java
@@ -13,12 +13,124 @@ public class MemberGrade {
     private double start;
     private double end;
     private Double increment;
-    private Double multiplypower;
-    private String mpstate;
-    private Double mpchangepower;
-    private Date updatetime;
-    private int versiont;
+    private Double multiplyPower;
+    private String mpState;
+    private Double mpChangePower;
+    private Date updateTime;
+    private int versionT;
     private Double discount;
-    private Date createtime;
-    private String creatorid;
+    private Date createTime;
+    private String creatorId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getStart() {
+        return start;
+    }
+
+    public void setStart(double start) {
+        this.start = start;
+    }
+
+    public double getEnd() {
+        return end;
+    }
+
+    public void setEnd(double end) {
+        this.end = end;
+    }
+
+    public Double getIncrement() {
+        return increment;
+    }
+
+    public void setIncrement(Double increment) {
+        this.increment = increment;
+    }
+
+    public Double getMultiplyPower() {
+        return multiplyPower;
+    }
+
+    public void setMultiplyPower(Double multiplyPower) {
+        this.multiplyPower = multiplyPower;
+    }
+
+    public String getMpState() {
+        return mpState;
+    }
+
+    public void setMpState(String mpState) {
+        this.mpState = mpState;
+    }
+
+    public Double getMpChangePower() {
+        return mpChangePower;
+    }
+
+    public void setMpChangePower(Double mpChangePower) {
+        this.mpChangePower = mpChangePower;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public int getVersionT() {
+        return versionT;
+    }
+
+    public void setVersionT(int versionT) {
+        this.versionT = versionT;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
 }
--- src/net/hzjxy/myshop/entity/MemberRechargeRecords.java
+++ src/net/hzjxy/myshop/entity/MemberRechargeRecords.java
@@ -8,10 +8,66 @@ import java.util.Date;
  */
 public class MemberRechargeRecords {
     private String id;
-    private String memberid;
-    private String membername;
+    private String memberId;
+    private String memberName;
     private double money;
-    private Date createtime;
+    private Date createTime;
     private String type;
-    private String shopid;
+    private String shopId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(String shopId) {
+        this.shopId = shopId;
+    }
 }
