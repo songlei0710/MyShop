2e6468f15279e7e8f9554d24d07b18dd743d359a
lgx123 <769948150@qq.com>
2014-12-04 10:41:12 +0800

    蔡立新

--- src/net/hzjxy/myshop/entity/MemberGrade.java
+++ src/net/hzjxy/myshop/entity/MemberGrade.java
@@ -8,17 +8,129 @@ import java.util.Date;
  */
 public class MemberGrade {
     private String id;
-    private String type;
-    private String name;
-    private double start;
-    private double end;
-    private Double increment;
-    private Double multiplypower;
+    private String typeA;
+    private String nameA;
+    private double startA;
+    private double endA;
+    private double incrementA;
+    private double multiplypower;
     private String mpstate;
-    private Double mpchangepower;
+    private double mpchangepower;
     private Date updatetime;
-    private int versiont;
-    private Double discount;
+    private int versiontA;
+    private double discount;
     private Date createtime;
     private String creatorid;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTypeA() {
+        return typeA;
+    }
+
+    public void setTypeA(String typeA) {
+        this.typeA = typeA;
+    }
+
+    public String getNameA() {
+        return nameA;
+    }
+
+    public void setNameA(String nameA) {
+        this.nameA = nameA;
+    }
+
+    public double getStartA() {
+        return startA;
+    }
+
+    public void setStartA(double startA) {
+        this.startA = startA;
+    }
+
+    public double getEndA() {
+        return endA;
+    }
+
+    public void setEndA(double endA) {
+        this.endA = endA;
+    }
+
+    public double getIncrementA() {
+        return incrementA;
+    }
+
+    public void setIncrementA(double incrementA) {
+        this.incrementA = incrementA;
+    }
+
+    public double getMultiplypower() {
+        return multiplypower;
+    }
+
+    public void setMultiplypower(double multiplypower) {
+        this.multiplypower = multiplypower;
+    }
+
+    public String getMpstate() {
+        return mpstate;
+    }
+
+    public void setMpstate(String mpstate) {
+        this.mpstate = mpstate;
+    }
+
+    public double getMpchangepower() {
+        return mpchangepower;
+    }
+
+    public void setMpchangepower(double mpchangepower) {
+        this.mpchangepower = mpchangepower;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public int getVersiontA() {
+        return versiontA;
+    }
+
+    public void setVersiontA(int versiontA) {
+        this.versiontA = versiontA;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getCreatorid() {
+        return creatorid;
+    }
+
+    public void setCreatorid(String creatorid) {
+        this.creatorid = creatorid;
+    }
 }
--- src/net/hzjxy/myshop/entity/MemberGroup.java
+++ src/net/hzjxy/myshop/entity/MemberGroup.java
@@ -8,11 +8,75 @@ import java.util.Date;
  */
 public class MemberGroup {
     private String id;
-    private String name;
+    private String nameA;
     private String status;
     private Date createtime;
     private String creatorid;
     private Date updatetime;
     private Integer versiont;
     private String attrs;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNameA() {
+        return nameA;
+    }
+
+    public void setNameA(String nameA) {
+        this.nameA = nameA;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getCreatorid() {
+        return creatorid;
+    }
+
+    public void setCreatorid(String creatorid) {
+        this.creatorid = creatorid;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public Integer getVersiont() {
+        return versiont;
+    }
+
+    public void setVersiont(Integer versiont) {
+        this.versiont = versiont;
+    }
+
+    public String getAttrs() {
+        return attrs;
+    }
+
+    public void setAttrs(String attrs) {
+        this.attrs = attrs;
+    }
 }
