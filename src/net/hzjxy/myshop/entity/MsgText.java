7d0f7c7b1f8bc0b9642a5cb55d72bb1204bdbccb
linchunlei <664390026@qq.com>
2014-12-04 09:40:02 +0800

    林春蕾--封装实体

--- src/net/hzjxy/myshop/entity/MsgText.java
+++ src/net/hzjxy/myshop/entity/MsgText.java
@@ -11,4 +11,27 @@ public class MsgText {
     private String text;
     private Date sendTime;
 
+    public String getMsgTextId() {
+        return msgTextId;
+    }
+
+    public void setMsgTextId(String msgTextId) {
+        this.msgTextId = msgTextId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(Date sendTime) {
+        this.sendTime = sendTime;
+    }
 }
