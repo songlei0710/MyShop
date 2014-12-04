17cdc0582887ab13d27aeae2b7d98de7e993e23e
liujiayin <506465308@qq.com>
2014-12-04 08:38:41 +0800

    刘佳音--实体修正x

--- src/net/hzjxy/myshop/entity/GoodsDetail.java
+++ src/net/hzjxy/myshop/entity/GoodsDetail.java
@@ -5,6 +5,30 @@ package net.hzjxy.myshop.entity;
  */
 public class GoodsDetail {
     private String id;
-    private String goodsid;
+    private String goodsId;
     private String detail;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
 }
--- src/net/hzjxy/myshop/entity/GoodsTypeBrand.java
+++ src/net/hzjxy/myshop/entity/GoodsTypeBrand.java
@@ -7,6 +7,46 @@ public class GoodsTypeBrand {
     private String goodsTypeBrandTid;
     private String goodsTypeId;
     private String name;
-    private String brandname;
-    private String brandid;
+    private String brandName;
+    private String brandId;
+
+    public String getGoodsTypeBrandTid() {
+        return goodsTypeBrandTid;
+    }
+
+    public void setGoodsTypeBrandTid(String goodsTypeBrandTid) {
+        this.goodsTypeBrandTid = goodsTypeBrandTid;
+    }
+
+    public String getGoodsTypeId() {
+        return goodsTypeId;
+    }
+
+    public void setGoodsTypeId(String goodsTypeId) {
+        this.goodsTypeId = goodsTypeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
 }
