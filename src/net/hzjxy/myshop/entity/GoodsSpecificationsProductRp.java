a31e2b68a523e8779a6a43acea54175f66802dfa
liujiayin <506465308@qq.com>
2014-12-09 10:23:01 +0800

    刘佳音

--- src/net/hzjxy/myshop/entity/GoodsSpecificationsProductRp.java
+++ src/net/hzjxy/myshop/entity/GoodsSpecificationsProductRp.java
@@ -5,7 +5,39 @@ package net.hzjxy.myshop.entity;
  */
 public class GoodsSpecificationsProductRp {
     private String goodsSpecificationsProductRpTid;
-    private String goodsid;
-    private String specidicationsid;
-    private String productid;
+    private String goodsId;
+    private String specidicationsId;
+    private String productId;
+
+    public String getGoodsSpecificationsProductRpTid() {
+        return goodsSpecificationsProductRpTid;
+    }
+
+    public void setGoodsSpecificationsProductRpTid(String goodsSpecificationsProductRpTid) {
+        this.goodsSpecificationsProductRpTid = goodsSpecificationsProductRpTid;
+    }
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getSpecidicationsId() {
+        return specidicationsId;
+    }
+
+    public void setSpecidicationsId(String specidicationsId) {
+        this.specidicationsId = specidicationsId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
 }
