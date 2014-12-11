094df0be39d6f398fa1c6fc5473c9fd2f2b3a54e
shyshy52134 <1412337472@qq.com>
2014-12-11 09:52:42 +0800

    第四组  生成xml

--- src/net/hzjxy/myshop/dao/PaymentDao.java
+++ src/net/hzjxy/myshop/dao/PaymentDao.java
@@ -56,7 +56,7 @@ public interface PaymentDao {
      * 根据state获取的支付方式
      * @return
      */
-    public List<Payment> findAllPayments(String state);
+    public List<Payment>(String state);
     /**
      * 根据id批量删除支付方式
      * @param str
