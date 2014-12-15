9be0a92201281bfaea6e56eebf884c42f6536731
liujiayin <506465308@qq.com>
2014-12-15 09:36:38 +0800

    刘明--Service

---
+++ src/net/hzjxy/myshop/service/impl/GoodsAttributeServiceImpl.java
@@ -0,0 +1,10 @@
+package net.hzjxy.myshop.service.impl;
+
+import net.hzjxy.myshop.service.GoodsAttributeRpService;
+import net.hzjxy.myshop.service.GoodsAttributeService;
+
+/**
+ * Created by Administrator on 2014/12/15.
+ */
+public class GoodsAttributeServiceImpl implements GoodsAttributeService {
+}
---
+++ src/net/hzjxy/myshop/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,9 @@
+package net.hzjxy.myshop.service.impl;
+
+import net.hzjxy.myshop.service.GoodsService;
+
+/**
+ * Created by Administrator on 2014/12/15.
+ */
+public class GoodsServiceImpl implements GoodsService {
+}
