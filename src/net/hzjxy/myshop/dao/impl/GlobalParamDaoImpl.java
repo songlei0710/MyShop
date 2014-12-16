0ada657c5ede94cb2b1768568fa047a66318d6aa
quyunjiao <978067760@qq.com>
2014-12-16 10:37:19 +0800

    曲云娇

---
+++ src/net/hzjxy/myshop/dao/impl/GlobalParamDaoImpl.java
@@ -0,0 +1,39 @@
+package net.hzjxy.myshop.dao.impl;
+
+
+import net.hzjxy.myshop.dao.GlobalParamDao;
+import net.hzjxy.myshop.entity.GlobalParam;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2014/12/16.
+ */
+public class GlobalParamDaoImpl implements GlobalParamDao{
+    @Override
+    public void updateGolbalParamByKey(GlobalParam gm){
+        SqlSession session=MybatisUtil.currentSession();
+        GlobalParamDao globalParamDao=session.getMapper(GlobalParamDao.class);
+        globalParamDao.updateGolbalParamByKey(gm);
+        session.commit();
+    }
+
+    @Override
+    public GlobalParam findValueByKey(String key) {
+        SqlSession session=MybatisUtil.currentSession();
+        GlobalParamDao globalParamDao=session.getMapper(GlobalParamDao.class);
+        globalParamDao.findValueByKey(key);
+        return globalParamDao.findValueByKey(key);
+    }
+
+    @Override
+    public List<GlobalParam> findAllGlobalParam() {
+        SqlSession session=MybatisUtil.currentSession();
+        GlobalParamDao globalParamDao=session.getMapper(GlobalParamDao.class);
+        globalParamDao.findAllGlobalParam();
+        return globalParamDao.findAllGlobalParam();
+    }
+
+
+}
