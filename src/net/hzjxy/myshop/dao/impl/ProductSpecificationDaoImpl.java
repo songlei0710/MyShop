0e8908238b2c68165d4ea65ef8b90ba83977216a
liujize <907284112@qq.com>
2014-12-16 11:01:33 +0800

    邵智奇  生成xml

--- src/net/hzjxy/myshop/dao/impl/ProductSpecificationDaoImpl.java
+++ src/net/hzjxy/myshop/dao/impl/ProductSpecificationDaoImpl.java
@@ -2,6 +2,7 @@ package net.hzjxy.myshop.dao.impl;
 
 import net.hzjxy.myshop.dao.ProductSpecificationDao;
 import net.hzjxy.myshop.entity.ProductSpecification;
+import org.apache.ibatis.session.SqlSession;
 
 import java.util.List;
 
@@ -11,41 +12,66 @@ import java.util.List;
 public class ProductSpecificationDaoImpl implements ProductSpecificationDao {
     @Override
     public int updateProductSpecification(ProductSpecification pst) {
-        return 0;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        int product=productSpecificationDao.updateProductSpecification(pst);
+        return product;
     }
 
     @Override
     public ProductSpecification findProductSpecificationsBySpecificationId(String specificationId) {
-        return null;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        ProductSpecification productSpecification=productSpecificationDao.findProductSpecificationsBySpecificationId(specificationId);
+        return productSpecification;
     }
 
     @Override
     public List<ProductSpecification> findAllProductSpecification(int currentPage, int lineSize) {
-        return null;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        List<ProductSpecification> productSpecificationList=productSpecificationDao.findAllProductSpecification(currentPage,lineSize);
+        return productSpecificationList;
     }
 
     @Override
     public int countFindAllProductSpecification() {
-        return 0;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        int product=productSpecificationDao.countFindAllProductSpecification();
+        return product;
     }
 
     @Override
     public int delProductSpecification(String[] list) {
-        return 0;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        productSpecificationDao.delProductSpecification(list);
+        session.commit();
+        return productSpecificationDao.delProductSpecification(list);
     }
 
     @Override
     public List<ProductSpecification> findAllProductSpecificati(String state) {
-        return null;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        List<ProductSpecification> productSpecificationList=productSpecificationDao.findAllProductSpecificati(state);
+        return productSpecificationList;
     }
 
     @Override
     public List<ProductSpecification> sortAllProductSpecification(int currentPage, int lineSize, String queryString) {
-        return null;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        List<ProductSpecification> productSpecificationList=productSpecificationDao.sortAllProductSpecification(currentPage,lineSize,queryString);
+        return productSpecificationList;
     }
 
     @Override
     public List<ProductSpecification> findAllProductSpecificationByGoodsTypeId(String goodsTypeId, String state) {
-        return null;
+        SqlSession session=MybatisUtil.currentSession();
+        ProductSpecificationDao productSpecificationDao=session.getMapper(ProductSpecificationDao.class);
+        List<ProductSpecification> productSpecificationList=productSpecificationDao.findAllProductSpecificationByGoodsTypeId(goodsTypeId,state);
+        return productSpecificationList;
     }
 }
