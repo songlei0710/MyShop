17271ca91c216ddd61145502a0214c9133c41ca4
liujiayin <506465308@qq.com>
2014-12-23 19:28:48 +0800

    赵翠翠

--- src/net/hzjxy/myshop/action/backstage/goods/GoodsAttributeRpAction.java
+++ src/net/hzjxy/myshop/action/backstage/goods/GoodsAttributeRpAction.java
@@ -4,4 +4,31 @@ package net.hzjxy.myshop.action.backstage.goods;
  * Created by Lenovo on 2014/12/23.
  */
 public class GoodsAttributeRpAction {
+    private String id;      //编号
+    private String goodsId; //商品编号
+    private String attrVal; //商品属性值
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getAttrVal() {
+        return attrVal;
+    }
+
+    public void setAttrVal(String attrVal) {
+        this.attrVal = attrVal;
+    }
 }
--- src/net/hzjxy/myshop/action/backstage/goods/GoodsBlinkedAction.java
+++ src/net/hzjxy/myshop/action/backstage/goods/GoodsBlinkedAction.java
@@ -1,7 +1,117 @@
 package net.hzjxy.myshop.action.backstage.goods;
 
+import java.util.Date;
+
 /**
  * Created by Lenovo on 2014/12/23.
  */
 public class GoodsBlinkedAction {
+    private String id;                      //编号
+    private String mainGoodsId;             //主商品Id
+    private String beLinkedProductInfo;     //关联商品信息
+    private String bMode;
+    private String state;
+    private String creatorId;
+    private Date createTime;
+    private Date updateTime;
+    private String sxLinkedGoodsId;
+    private String mainProductId;
+    private String shopId;
+    private String mainGoodsName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMainGoodsId() {
+        return mainGoodsId;
+    }
+
+    public void setMainGoodsId(String mainGoodsId) {
+        this.mainGoodsId = mainGoodsId;
+    }
+
+    public String getBeLinkedProductInfo() {
+        return beLinkedProductInfo;
+    }
+
+    public void setBeLinkedProductInfo(String beLinkedProductInfo) {
+        this.beLinkedProductInfo = beLinkedProductInfo;
+    }
+
+    public String getbMode() {
+        return bMode;
+    }
+
+    public void setbMode(String bMode) {
+        this.bMode = bMode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getSxLinkedGoodsId() {
+        return sxLinkedGoodsId;
+    }
+
+    public void setSxLinkedGoodsId(String sxLinkedGoodsId) {
+        this.sxLinkedGoodsId = sxLinkedGoodsId;
+    }
+
+    public String getMainProductId() {
+        return mainProductId;
+    }
+
+    public void setMainProductId(String mainProductId) {
+        this.mainProductId = mainProductId;
+    }
+
+    public String getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(String shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getMainGoodsName() {
+        return mainGoodsName;
+    }
+
+    public void setMainGoodsName(String mainGoodsName) {
+        this.mainGoodsName = mainGoodsName;
+    }
 }
