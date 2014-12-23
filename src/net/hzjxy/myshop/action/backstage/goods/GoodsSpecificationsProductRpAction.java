f91b149aa670ff8b6bfed20b054b5e269ed0109a
liujiayin <506465308@qq.com>
2014-12-23 19:33:49 +0800

    刘明

--- src/net/hzjxy/myshop/action/backstage/goods/GoodsSpecificationsProductRpAction.java
+++ src/net/hzjxy/myshop/action/backstage/goods/GoodsSpecificationsProductRpAction.java
@@ -4,4 +4,40 @@ package net.hzjxy.myshop.action.backstage.goods;
  * Created by Lenovo on 2014/12/23.
  */
 public class GoodsSpecificationsProductRpAction {
+    private String goodsSpecificationsProductRpTid;
+    private String goodsId;
+    private String specificationsId;
+    private String productId;
+
+    public String getGoodsSpecificationsProductRpTid() {
+        return goodsSpecificationsProductRpTid;
+    }
+
+    public void setGoodsSpecificationsProductRpTid(String goodsSpecificationsProductRpTid) {
+        this.goodsSpecificationsProductRpTid = goodsSpecificationsProductRpTid;
+    }
+
+    public String getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(String goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getSpecificationsId() {
+        return specificationsId;
+    }
+
+    public void setSpecificationsId(String specificationsId) {
+        this.specificationsId = specificationsId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
 }
--- src/net/hzjxy/myshop/action/backstage/goods/GoodsTypeAction.java
+++ src/net/hzjxy/myshop/action/backstage/goods/GoodsTypeAction.java
@@ -1,7 +1,54 @@
 package net.hzjxy.myshop.action.backstage.goods;
 
+import java.util.Date;
+
 /**
  * Created by Lenovo on 2014/12/23.
  */
 public class GoodsTypeAction {
+    private String goodsTypeId;
+    private String name;
+    private Date createTime;
+    private String creatorId;
+    private String goodsParameter;
+
+    public String getGoodsTypeId() {
+        return goodsTypeId;
+    }
+
+    public void setGoodsTypeId(String goodsTypeId) {
+        this.goodsTypeId = goodsTypeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public String getGoodsParameter() {
+        return goodsParameter;
+    }
+
+    public void setGoodsParameter(String goodsParameter) {
+        this.goodsParameter = goodsParameter;
+    }
 }
