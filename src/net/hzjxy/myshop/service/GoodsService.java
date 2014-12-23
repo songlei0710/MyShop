080793f7c4fe893b80bb813fb80465990d44b0ba
liujiayin <506465308@qq.com>
2014-12-23 19:13:18 +0800

    刘佳音

--- src/net/hzjxy/myshop/service/GoodsService.java
+++ src/net/hzjxy/myshop/service/GoodsService.java
@@ -1,8 +1,23 @@
 package net.hzjxy.myshop.service;
 
+import net.hzjxy.myshop.entity.Goods;
+
 /**
  * Created by Administrator on 2014/12/15.
  */
 public interface GoodsService {
+    /**
+     * 更新商品
+     *
+     * @param goods
+     * @return
+     */
+    public void updateGoods(Goods goods);
+    /**
+     * 添加虚拟商品
+     * @param goods
+     * @return
+     */
+    public int addGoods(Goods goods);
 
 }
