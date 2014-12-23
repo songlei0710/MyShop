d2fa0a42cf5624d608bb9a6eb094c9488ae2a0ad
zhaocuicui <1563536856@qq.com>
2014-12-23 11:19:23 +0800

    赵翠翠-

--- web/admin/js/goods/goodscommentmentjs.js
+++ web/admin/js/goods/goodscommentmentjs.js
@@ -1,5 +1,6 @@
 $(function(){
     $('#submit').click(function(){
+        var commentContent=$('#commentContent').val();
       var replyorcommentusername=$('#replyorcommentusername').val();
         if(replyorcommentusername==""){
             formwarning("#alerterror","请填写商品评论");
@@ -9,10 +10,54 @@ $(function(){
             "replyorcommentusername":replyorcommentusername
         },function(data){
             if(data.sucflag){
-                window.location.href="goodscommentment.jsp";
+                window.location.href="goodscomment.jsp";
             }
         });
-
+        this.value = "提交中";
+        this.disabled = true;
+        $.post("addGoodsComment.action", {
+            "replyorcommentusername": replyorcommentusername
+        }, function (data) {
+            if (data.sucflag) {
+                window.location.href = "goodscomment.jsp";
+            } else {
+                formwarning("#alerterror", "商品评论增加失败");
+                return false;
+            }
+        });
+    });
+    findGoodsCommentById=function(){
+        var commentId= $.query.get('commentId');
+        if(commentId==""){
+            return false;
+        }
+        $.post("findArticleByArticleId.action",{"articleId":articleId},function(data){
+            if(data.bean!=null){
+                $("#replyorcommentusername").val(date.bean.replyorcommentusername);
+                KE.html("commentContent",data.bean.commentContent);
+            }
+        });
+    }
+    /**
+     * 更新文章
+     * @returns
+     */
+    updateGoodsComment=function(){
+        var commentContent = $("#commentContent").val();
+        var replyorcommentusername = $("#replyorcommentusername").val();
+        $.post("updateGoodsComment.action", {
+            "replyorcommentusername": replyorcommentusername
+        },function(data){
+            if (data.sucflag) {
+                window.location.href = "goodscomment.jsp";
+            } else {
+                formwarning("#alerterror", "商品评论更新失败");
+                return false;
+            }
+        });
+    }
+    $("#update").click(function(){
+        updateGoodsComment();
     });
 })
 /*
