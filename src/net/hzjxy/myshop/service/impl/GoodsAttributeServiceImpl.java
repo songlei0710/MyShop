ddd3627c8684c40ecdf1442af6b7ddc23e6fd435
liujiayin <506465308@qq.com>
2014-12-23 08:17:01 +0800

    刘佳音-jsp改

--- src/net/hzjxy/myshop/service/impl/GoodsAttributeServiceImpl.java
+++ src/net/hzjxy/myshop/service/impl/GoodsAttributeServiceImpl.java
@@ -1,5 +1,6 @@
 package net.hzjxy.myshop.service.impl;
 
+import net.hzjxy.myshop.entity.GoodsAttribute;
 import net.hzjxy.myshop.service.GoodsAttributeRpService;
 import net.hzjxy.myshop.service.GoodsAttributeService;
 
@@ -7,4 +8,17 @@ import net.hzjxy.myshop.service.GoodsAttributeService;
  * Created by Administrator on 2014/12/15.
  */
 public class GoodsAttributeServiceImpl implements GoodsAttributeService {
+    public GoodsAttributeServiceImpl() {
+        super();
+    }
+
+    @Override
+    public int addGoodsAttribute(GoodsAttribute goodsAttribute) {
+        return 0;
+    }
+
+    @Override
+    public int updateGoodsAttribute(GoodsAttribute goodsAttribute) {
+        return 0;
+    }
 }
