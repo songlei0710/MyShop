0e1fff3fbb888be1150378723393b6e1bf2d0894
liujiayin <506465308@qq.com>
2014-12-23 19:15:37 +0800

    邵亚楠

--- src/net/hzjxy/myshop/service/impl/GoodsServiceImpl.java
+++ src/net/hzjxy/myshop/service/impl/GoodsServiceImpl.java
@@ -1,9 +1,30 @@
 package net.hzjxy.myshop.service.impl;
 
+import net.hzjxy.myshop.dao.GoodsDao;
+import net.hzjxy.myshop.entity.Goods;
 import net.hzjxy.myshop.service.GoodsService;
 
 /**
  * Created by Administrator on 2014/12/15.
  */
 public class GoodsServiceImpl implements GoodsService {
+private GoodsDao goodsDao;
+
+    public GoodsDao getGoodsDao() {
+        return goodsDao;
+    }
+
+    public void setGoodsDao(GoodsDao goodsDao) {
+        this.goodsDao = goodsDao;
+    }
+
+    @Override
+    public void updateGoods(Goods goods) {
+this.goodsDao.updateGoods(goods);
+    }
+
+    @Override
+    public int addGoods(Goods goods) {
+        return this.goodsDao.addGoods(goods);
+    }
 }
