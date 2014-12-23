0e30d641ce7e299d240f7c58e007b01ad9d5c881
liujiayin <506465308@qq.com>
2014-12-23 20:09:14 +0800

    刘明

--- src/net/hzjxy/myshop/service/impl/GoodsCategoryServiceImpl.java
+++ src/net/hzjxy/myshop/service/impl/GoodsCategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package net.hzjxy.myshop.service.impl;
 
+import net.hzjxy.myshop.dao.GoodsCategoryDao;
 import net.hzjxy.myshop.entity.GoodsCategory;
 import net.hzjxy.myshop.service.GoodsCategoryService;
 
@@ -7,17 +8,23 @@ import net.hzjxy.myshop.service.GoodsCategoryService;
  * Created by Administrator on 2014/12/15.
  */
 public class GoodsCategoryServiceImpl implements GoodsCategoryService {
-    public GoodsCategoryServiceImpl() {
-        super();
+    private net.hzjxy.myshop.dao.GoodsCategoryDao GoodsCategoryDao;
+
+    public GoodsCategoryDao getGoodsCategoryDao() {
+        return GoodsCategoryDao;
+    }
+
+    public void setGoodsCategoryDao(GoodsCategoryDao goodsCategoryDao) {
+        GoodsCategoryDao = goodsCategoryDao;
     }
 
     @Override
     public int updateGoodscategoryT(GoodsCategory gct) {
-        return 0;
+        return this.getGoodsCategoryDao().updateGoodscategoryT(gct);
     }
 
     @Override
     public int addGoodsCategory(GoodsCategory goodsCategory) {
-        return 0;
+        return this.getGoodsCategoryDao().addGoodsCategory(goodsCategory);
     }
 }
