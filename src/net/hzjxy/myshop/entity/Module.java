dbf68da874d81f5b6f9623310ef991eef61d9305
wendy521 <www.wendys_l@qq.com>
2014-12-23 11:33:42 +0800

    5组 实体温东源

--- src/net/hzjxy/myshop/entity/Module.java
+++ src/net/hzjxy/myshop/entity/Module.java
@@ -10,4 +10,28 @@ public class Module {
     private String id;//组件id
 private String modulename;//组件名称
 private Date createtime;//创建时间
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModulename() {
+        return modulename;
+    }
+
+    public void setModulename(String modulename) {
+        this.modulename = modulename;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
 }
